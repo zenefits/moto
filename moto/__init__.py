d2bebcf0e5752bbaf0db1dbc116e9b6d56433a60
Kefu Zhao <kzhao@zenefits.com>
2016-05-11 14:13:32 -0700

    Cut backends due to old boto version we are using

--- moto/__init__.py
+++ moto/__init__.py
@@ -3,35 +3,9 @@ import logging
 logging.getLogger('boto').setLevel(logging.CRITICAL)
 
 __title__ = 'moto'
-__version__ = '0.4.24'
+__version__ = '0.4.24.1'
 
-from .apigateway import mock_apigateway  # flake8: noqa
-from .autoscaling import mock_autoscaling  # flake8: noqa
-from .awslambda import mock_lambda  # flake8: noqa
-from .cloudformation import mock_cloudformation  # flake8: noqa
-from .cloudwatch import mock_cloudwatch  # flake8: noqa
-from .datapipeline import mock_datapipeline  # flake8: noqa
-from .dynamodb import mock_dynamodb  # flake8: noqa
-from .dynamodb2 import mock_dynamodb2  # flake8: noqa
-from .ec2 import mock_ec2  # flake8: noqa
-from .ecs import mock_ecs  # flake8: noqa
-from .elb import mock_elb  # flake8: noqa
-from .emr import mock_emr  # flake8: noqa
-from .glacier import mock_glacier  # flake8: noqa
-from .opsworks import mock_opsworks  # flake8: noqa
-from .iam import mock_iam  # flake8: noqa
-from .kinesis import mock_kinesis  # flake8: noqa
-from .kms import mock_kms  # flake8: noqa
-from .rds import mock_rds  # flake8: noqa
-from .rds2 import mock_rds2  # flake8: noqa
-from .redshift import mock_redshift  # flake8: noqa
-from .s3 import mock_s3  # flake8: noqa
-from .s3bucket_path import mock_s3bucket_path  # flake8: noqa
-from .ses import mock_ses  # flake8: noqa
-from .sns import mock_sns  # flake8: noqa
-from .sqs import mock_sqs  # flake8: noqa
-from .sts import mock_sts  # flake8: noqa
-from .route53 import mock_route53  # flake8: noqa
+
 from .swf import mock_swf  # flake8: noqa
 
 
